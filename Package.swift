4ee6de64d7ebccba26b7437a2346840a1b3b41fd
noppe <noppelabs@gmail.com>
2025-06-16 00:08:30 +0900

    remove HTMLEntities

--- Package.swift
+++ Package.swift
@@ -12,17 +12,11 @@ let package = Package(
             name: "DocumentDecoder",
             targets: ["DocumentDecoder"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Kitura/swift-html-entities", from: "4.0.1"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DocumentDecoder",
-            dependencies: [
-                .product(name: "HTMLEntities", package: "swift-html-entities")
-            ]
+            name: "DocumentDecoder"
         ),
         .testTarget(
             name: "DocumentDecoderTests",
